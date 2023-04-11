f76ace240ab6cb9e7dafc3195acb84066a6d6a31
MSameer192 <sameer192.official@gmail.com>
2023-04-11 20:18:33 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Sameer
+          Learn React with Sameer Memon
         </a>
       </header>
     </div>
